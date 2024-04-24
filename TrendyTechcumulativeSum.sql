b7428d868d9219cf94738e372f3ed2b1755465d4
marreddy <marreddypothi@gmail.com>
2024-04-24 20:56:03 +0530

    Update TrendyTechcumulativeSum.sql

--- TrendyTechcumulativeSum.sql
+++ TrendyTechcumulativeSum.sql
@@ -28,7 +28,7 @@ mysql> SELECT * FROM transactions;
 |              6 |          1 | 2024-01-04 |        90 |
 +----------------+------------+------------+-----------+
 6 rows in set (0.00 sec)  
---EXPECTED OUTPUT
+--EXPECTED OUTPUT 
 +----------------+------------+------------+-----------+----------------+
 | transaction_id | product_id | sales_date | sales_amt | cumulative_sum |
 +----------------+------------+------------+-----------+----------------+
@@ -39,4 +39,26 @@ mysql> SELECT * FROM transactions;
 |              5 |          2 | 2024-01-02 |       180 |            180 |
 |              6 |          1 | 2024-01-04 |        90 |             90 |
 +----------------+------------+------------+-----------+----------------+
+
+
+mysql> SELECT transaction_id, product_id, sales_date, sales_amt,
+  SUM(sales_amt) OVER(PARTITION BY seq ORDER BY product_id, sales_date) cumulative_sum FROM
+  (
+  SELECT transaction_id, product_id, sales_date, sales_amt, IF(product_id = lg , @seq, @seq:= @seq + 1) seq FROM (
+  SELECT transaction_id, product_id, sales_date, sales_amt, LAG(product_id , 1, product_id) OVER() lg, @seq:=1   FROM transactions
+  ) tab
+  ) t1 ORDER BY product_id, sales_date;
+  
+----EXPECTED OUTPUT ORDER BY product_id, sales_date
++----------------+------------+------------+-----------+----------------+
+| transaction_id | product_id | sales_date | sales_amt | cumulative_sum |
++----------------+------------+------------+-----------+----------------+
+|              1 |          1 | 2024-01-01 |       100 |            100 |
+|              2 |          1 | 2024-01-02 |       150 |            250 |
+|              4 |          1 | 2024-01-03 |       120 |            120 |
+|              6 |          1 | 2024-01-04 |        90 |             90 |
+|              3 |          2 | 2024-01-01 |       200 |            200 |
+|              5 |          2 | 2024-01-02 |       180 |            180 |
++----------------+------------+------------+-----------+----------------+
+6 rows in set, 2 warnings (0.00 sec)
 
